9d5c42b56c371b0de783c60aeef38dc1943d8a92
Viraj <340viraj@gmail.com>
2026-02-14 23:55:27 -0500

    Undo static page changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Static export
-  basePath: '/personal-website', // GitHub Pages base path
-  assetPrefix: '/personal-website/', // For correct asset loading
+  
 }
 
 module.exports = nextConfig
